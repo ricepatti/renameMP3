859686cdd666f8fa8cfb1fd97705f060e177e20f
unknown <mungusy@gmail.com>
2025-08-23 14:25:30 -0700

    Works!

--- renamemp3.py
+++ renamemp3.py
@@ -13,14 +13,6 @@ mainFolder = r"F:\test"
 
 
 # Here are the functions that the code uses.
-def getmusicfiles(folder):
-    """
-    Returns all files.
-    """    
-    all_entries = os.listdir(folder)
-    allfiles = [entry for entry in all_entries if os.path.isfile(os.path.join(folder, entry))]
-    return [entry for entry in allfiles if os.path.splitext(allfiles)[1] == ".mp3"]
-
 def get_mp3_files_glob(directory_path):
     """
     Returns a list of MP3 files in the specified directory.
@@ -42,26 +34,14 @@ folderList = has_subfolders_listdir(mainFolder)
 
 for folder in folderList:
     fileList = get_mp3_files_glob(f"{mainFolder}\{folder}")
-    for file in fileList:
-        metadata = EasyID3(file)
-
-    
-
-# Code below for reference only.
-curFile = r"F:\test\F41\POAF.mp3"
-curPath = os.path.dirname(curFile)
-
-audio = EasyID3(curFile)
-
-artist = audio.get("artist")[0]
-title = audio.get("title")[0]
-album = audio.get("album")[0]
-
-newName = f"{curPath}\{artist} - {title}.mp3"
-
-os.rename(curFile,newName)
-
-    
-    
-    
-    
+    for curFile in fileList:
+        audio = EasyID3(curFile)
+        try:
+            artist = audio.get("artist")[0]
+            title = audio.get("title")[0]
+            curPath = os.path.dirname(curFile)
+            newName = f"{curPath}\{artist} - {title}.mp3"
+            os.rename(curFile,newName)
+        except:
+            print("Artist and title not found. Skipping file.")
+            
